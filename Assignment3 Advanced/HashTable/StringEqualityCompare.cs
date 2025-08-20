ef3e13f7ec417028906d7df16882ead4d1c614ee
saramohamed512 <saramohamed12656@gmail.com>
2025-08-20 20:30:15 +0200

    Non-Generic [HashTable]

---
+++ Assignment3 Advanced/HashTable/StringEqualityCompare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_Advanced.HashTable
+{
+    internal class StringEqualityCompare : IEqualityComparer
+    {
+        public new bool Equals(object? x, object? y)
+        {
+            string? X = x as string;
+            string? Y = y as string;
+            return X?.ToLower().Equals(Y?.ToLower()) ?? false;
+        }
+
+        public int GetHashCode(object obj)
+        {
+           String? value= obj as String;
+            return value?.ToLower().GetHashCode() ?? 0;
+        }
+    }
+}
--- Assignment3 Advanced/Program.cs
+++ Assignment3 Advanced/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assignment3_Advanced.HashTable;
+using System;
+using System.Collections;
 
 namespace MyApp
 {
@@ -6,7 +8,47 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region HashTable
+          
+            
+            Hashtable PhoneNote = new Hashtable(new StringEqualityCompare()) {
+                //{ "Mona",123},
+                //{ "Ali", 456},
+                //{ "Sara", 789 }
+
+                //using indexer
+                ["Mona"]=123,
+                ["Ali"]=456,
+                ["Sara"]=789
+            };
+
+            //PhoneNote.Add("Mona",123);
+            //PhoneNote.Add("Ali", 456);
+            //PhoneNote.Add("Sara", 789);
+            //foreach (object o in PhoneNote) {
+            //    Console.WriteLine(o);
+            //}
+            #region Indexer
+            //Console.WriteLine(PhoneNote["Mohamed"]);
+            //PhoneNote["Mohamed"] = 244;
+            //PhoneNote["Mena"] = 478;
+
+            #endregion
+            if (!PhoneNote.Contains("Ali"))
+            {
+                PhoneNote.Add("ali", 456);
+            }
+            else
+            {
+                Console.WriteLine("Key already existed");
+            }
+
+            //print specified data =>DictionaryEntry
+            foreach (DictionaryEntry entry in PhoneNote)
+            {
+                Console.WriteLine($"name: {entry.Key} | phone number: {entry.Value}");
+            }
+            #endregion
         }
     }
 }
